cca534600dfdca18cf2d2ffa99c91585fa641d26
Stangar999 <lockinout@mail.ru>
2023-01-27 15:02:57 +0300

    after review

---
+++ src/read_input.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "data_handler.h"
+
+[[maybe_unused]] static std::vector<std::vector<std::string>> ReadInput(std::istream& in) {
+    std::vector<std::vector<std::string> > r;
+    for (std::string line; std::getline(in, line);)
+    {
+        r.emplace_back(Split(line, '\t'));
+    }
+    return r;
+}
